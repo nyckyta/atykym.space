fe64057c8e921278374d93454b3af42375d3dc64
Mykyta Leonov <nykyta.leonov@gmail.com>
2025-10-09 14:56:31 +0300

    Add ls functionality

--- app/cmd/cmd.go
+++ app/cmd/cmd.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"html/template"
 	"io"
+	"io/fs"
+	"log"
 	"os"
 	"path"
 	"strings"
@@ -12,28 +14,67 @@ import (
 const ERROR_TEMPLATE_PATH = "templates/error.html"
 const CAT_OUTPUT_PATH = "templates/cat-output.html"
 const HELP_OUTPUT_PATH = "templates/help.html"
+const LS_OUTPUT_PATH = "templates/ls-output.html"
 
 type Cmd struct {
-	Cmd string
-	Params[] string
+	Cmd    string
+	Params []string
 }
 
 type CmdErr struct {
-	Cmd string
+	Cmd    string
 	ErrMsg string
 }
 
 type CommandRunner interface {
-	Run() string;
+	Run() string
 }
 
 func (cmd Cmd) Run() string {
-	switch(cmd.Cmd) {
-	case "cat": return handleCat(cmd)
-	case "help": return handleHelp()
-	case "": return ""
-	default: return handleUnknown(cmd)
+	switch cmd.Cmd {
+	case "cat":
+		return handleCat(cmd)
+	case "ls":
+		return handleLs(cmd)
+	case "help":
+		return handleHelp()
+	case "":
+		return ""
+	default:
+		return handleUnknown(cmd)
 	}
+}
+
+func handleLs(cmd Cmd) string {
+	var directoryToSearch string
+	if len(cmd.Params) < 1 {
+		directoryToSearch = "."
+	}
+	files, err := lsGetFiles(directoryToSearch)
+
+	if err != nil {
+		log.Printf("[ERROR] Failed to get list of files")
+		return executeTemplateAgainstAny(ERROR_TEMPLATE_PATH, "Failed to read directory")
+	}
+	return executeTemplateAgainstAny(LS_OUTPUT_PATH, files)
+
+}
+
+func lsGetFiles(dir string) ([]string, error) {
+	_fs := os.DirFS("anon/home").(fs.ReadDirFS)
+	path := path.Clean(dir)
+	files, err := _fs.ReadDir(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	filesInDirectory := make([]string, len(files))
+	for i, v := range files {
+		filesInDirectory[i] = v.Name()
+	}
+
+	return filesInDirectory, nil
 }
 
 func handleUnknown(cmd Cmd) string {
@@ -47,7 +88,7 @@ func handleHelp() string {
 
 func handleCat(cmd Cmd) string {
 	params := cmd.Params
-	if (len(params) < 1) {
+	if len(params) < 1 {
 		err := CmdErr{cmd.Cmd, "no file provided."}
 		return executeTemplateAgainstAny(ERROR_TEMPLATE_PATH, err)
 	}
@@ -79,11 +120,10 @@ func handleCat(cmd Cmd) string {
 	return executeTemplateAgainstAny(CAT_OUTPUT_PATH, string(fileContent))
 }
 
-
 func executeTemplateAgainstAny(pathToTemplate string, vars any) string {
 	t, e := template.ParseFiles(pathToTemplate)
 	if e != nil {
-		panic("Failed to parse unknown command template")
+		panic("Failed to parse unknown command template, " + e.Error())
 	}
 	b := strings.Builder{}
 	w := bufio.NewWriter(&b)
@@ -96,26 +136,26 @@ func executeTemplateAgainstAny(pathToTemplate string, vars any) string {
 }
 
 /*
-	Converts string to Command that is able to run.
+Converts string to Command that is able to run.
 */
 func ToCommand(input string) CommandRunner {
 	tokens := strings.Fields(input)
-	if (len(tokens) > 1) {
+	if len(tokens) > 1 {
 		return Cmd{
-			Cmd: tokens[0],
+			Cmd:    tokens[0],
 			Params: tokens[1:],
 		}
 	}
 
-	if (len(tokens) == 1) {
+	if len(tokens) == 1 {
 		return Cmd{
-			Cmd: tokens[0],
+			Cmd:    tokens[0],
 			Params: []string{},
 		}
 	}
 
 	return Cmd{
-		Cmd: "",
+		Cmd:    "",
 		Params: []string{},
 	}
 }
